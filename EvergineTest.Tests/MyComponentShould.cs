9f8c86e6ab0c6064cc4a31d564d431ad0e4f0bf6
Marcos Cobeña Morián <mcobena@plainconcepts.com>
2022-12-22 15:02:15 +0100

    add BindComponent/Service tests

--- EvergineTest.Tests/MyComponentShould.cs
+++ EvergineTest.Tests/MyComponentShould.cs
@@ -1,4 +1,5 @@
 using Evergine.Framework;
+using Evergine.Framework.Graphics;
 using Evergine.Mocks;
 using Xunit;
 
@@ -6,18 +7,36 @@ namespace EvergineTest.Tests
 {
     public class MyComponentShould
     {
+        private readonly MyComponent component;
+
+        private readonly MockWindowsSystem windowsSystem;
+
+        public MyComponentShould()
+        {
+            this.component = new MyComponent();
+            var entity = new Entity()
+                .AddComponent(component)
+                .AddComponent(new Transform3D());
+            var scene = new MockScene();
+            scene.Add(entity);
+            var application = new MyApplication();
+            this.windowsSystem = MockWindowsSystem.Create(application, scene);
+        }
+        [Fact]
+        public void KeepMyBooleanPropertyFalseBeforeStarting()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.False(this.component.MyBooleanProperty);
+        }
         [Fact]
         public void ChangeMyBooleanPropertyToTrueOnStart()
         {
             // Arrange
-            var component = new MyComponent();
-            var entity = new Entity()
-                .AddComponent(component);
-            var scene = new MockScene();
-            scene.Add(entity);
-            var application = new MyApplication();
-            var windowsSystem = MockWindowsSystem.Create(application, scene);
-
+            
             // Act
             windowsSystem.RunOneLoop();
 
--- EvergineTest/MyComponent.cs
+++ EvergineTest/MyComponent.cs
@@ -1,9 +1,18 @@
-﻿using Evergine.Framework;
+﻿using Evergine.Common.Graphics;
+using Evergine.Framework;
+using Evergine.Framework.Graphics;
+using System.Diagnostics;
 
 namespace EvergineTest
 {
     public class MyComponent : Component
     {
+        [BindComponent]
+        protected Transform3D transform3D;
+
+        [BindService]
+        protected GraphicsContext graphicsContext;
+
         public bool MyBooleanProperty { get; private set; }
 
         protected override void Start()
@@ -11,6 +20,8 @@ namespace EvergineTest
             base.Start();
 
             this.MyBooleanProperty = true;
+            this.transform3D.Scale *= 2;
+            Debug.WriteLine(this.graphicsContext.BackendType);
         }
     }
 }
