e56dcca73db8fd2727cdfabe45fdae2d7045bb68
Marcos Cobeña Morián <mcobena@plainconcepts.com>
2025-02-25 12:41:14 +0100

    minor refactor

--- src/Evergine.Mocks/MockWindowsSystem.cs
+++ src/Evergine.Mocks/MockWindowsSystem.cs
@@ -46,29 +46,7 @@ namespace Evergine.Mocks
 
         public static MockWindowsSystem Create(Application application, Scene scene)
         {
-            var instance = new MockWindowsSystem();
-            application.Container.RegisterInstance(instance);
-            const uint width = 1280;
-            const uint height = 720;
-            var window = instance.CreateWindow(string.Empty, width, height);
-            var graphicsContext = new MockGraphicsContext();
-            graphicsContext.CreateDevice();
-            var swapChainDescription = new SwapChainDescription()
-            {
-                ColorTargetFormat = PixelFormat.R8G8B8A8_UNorm,
-                ColorTargetFlags = TextureFlags.RenderTarget | TextureFlags.ShaderResource,
-                DepthStencilTargetFormat = PixelFormat.D24_UNorm_S8_UInt,
-                DepthStencilTargetFlags = TextureFlags.DepthStencil,
-                SampleCount = TextureSampleCount.None,
-                RefreshRate = 60,
-                Width = width,
-                Height = height,
-            };
-            var swapChain = graphicsContext.CreateSwapChain(swapChainDescription);
-            var graphicsPresenter = application.Container.Resolve<GraphicsPresenter>();
-            var firstDisplay = new Display(window, swapChain);
-            graphicsPresenter.AddDisplay("DefaultDisplay", firstDisplay);
-            application.Container.RegisterInstance(graphicsContext);
+            var instance = CreateWindowsSystem(application);
             instance.Run(
                 () =>
                 {
@@ -89,6 +67,34 @@ namespace Evergine.Mocks
         public static MockWindowsSystem Create<TScene>(Application application, Guid sceneAssetId)
             where TScene : Scene
         {
+            var instance = CreateWindowsSystem(application);
+            instance.Run(
+                () =>
+                {
+                    application.Initialize();
+                    var assetsService = application.Container.Resolve<AssetsService>();
+                    var scene = assetsService.Load<TScene>(sceneAssetId);
+                    var screenContextManager = application.Container.Resolve<ScreenContextManager>();
+                    var screenContext = new ScreenContext(scene);
+                    screenContextManager.To(screenContext);
+                },
+                () =>
+                {
+                    application.UpdateFrame(nextRenderCallbackElapsedTime!.Value);
+                    application.DrawFrame(nextRenderCallbackElapsedTime.Value);
+                });
+
+            return instance;
+        }
+
+        protected override void CreateLoopThread(Action loadAction, Action renderCallback)
+        {
+            loadAction();
+            this.renderCallback = renderCallback;
+        }
+
+        private static MockWindowsSystem CreateWindowsSystem(Application application)
+        {
             var instance = new MockWindowsSystem();
             application.Container.RegisterInstance(instance);
             const uint width = 1280;
@@ -112,29 +118,8 @@ namespace Evergine.Mocks
             var firstDisplay = new Display(window, swapChain);
             graphicsPresenter.AddDisplay("DefaultDisplay", firstDisplay);
             application.Container.RegisterInstance(graphicsContext);
-            instance.Run(
-                () =>
-                {
-                    application.Initialize();
-                    var assetsService = application.Container.Resolve<AssetsService>();
-                    var scene = assetsService.Load<TScene>(sceneAssetId);
-                    var screenContext = new ScreenContext(scene);
-                    var screenContextManager = application.Container.Resolve<ScreenContextManager>();
-                    screenContextManager.To(screenContext);
-                },
-                () =>
-                {
-                    application.UpdateFrame(nextRenderCallbackElapsedTime!.Value);
-                    application.DrawFrame(nextRenderCallbackElapsedTime.Value);
-                });
 
             return instance;
-        }
-
-        protected override void CreateLoopThread(Action loadAction, Action renderCallback)
-        {
-            loadAction();
-            this.renderCallback = renderCallback;
         }
     }
 }
